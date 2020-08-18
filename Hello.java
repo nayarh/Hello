c64317ca1c87bef5e851aa9aad658bf6116b71f9
nayarh <69828676+nayarh@users.noreply.github.com>
2020-08-17 21:24:29 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*******************************************************
+*Notepad++
+********************************************************
+*Yaa Adarkwa Darko
+*August 17th 2020
+*CMSC 255 C90
+********************************************************/
+
+public class Hello{
+
+	public static void main(String[] args) {
+	
+	 System.out.println("<Yaa Adarkwa Darko>");
+	 System.out.println("<Project Java>");
+	 System.out.println("<Installing Java and all associated applications>");
+	 System.out.println("<7/28/2020>");
+	 System.out.println("<CMSC 255 C90>");
+	 System.out.println();
+	 }
+}
